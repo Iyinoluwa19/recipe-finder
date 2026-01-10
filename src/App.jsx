506da54cb182d57d2aa4c9a8a91662a2622d2e3e
Iyinoluwa19 <idowuiyin@gmail.com>
2026-01-10 23:12:16 +0100

    designing navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import Navbar from "./components/navbar";
-// import RecipeCard from "./components/recipeCard";
+import RecipeCard from "./components/recipeCard";
 
 
 function App() {
 
 
   return (
-    <div>
+    <div className="mx-6 my-4 bg-gray-100">
       <Navbar />
-      {/* <RecipeCard /> */}
+      <RecipeCard />
     </div>
   )
 }
--- src/components/recipeCard.jsx
+++ src/components/recipeCard.jsx
@@ -1,11 +1,16 @@
-import { nigerianRecipes } from '../data/dishes';
+import { nigerianRecipes } from '../data/dishes.jsx';
 
 export default function RecipeCard() {
+    console.log(nigerianRecipes);
     return(
         <div>
             {nigerianRecipes.map((recipe) => (
                 <div key={recipe.id}>
                     <h2>{recipe.name}</h2>
+
+                {recipe.ingredients.map((ingredient, index) => (
+                    <li key={index}>{ingredient}</li>
+                ))}
                 </div>
             ))}
         </div>
